08492c8cc61c59ad2330c8f0e3fa7ecc8ba7e0a3
Akali <Akali@My-PC>
2020-10-10 14:10:46 +0700

    Multiple Controller

---
+++ MultipleController/src/main/java/com/nguyenthanhhau/multiplecontroller/MVCController.java
@@ -0,0 +1,15 @@
+package com.nguyenthanhhau.multiplecontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MVCController {
+	@RequestMapping("/mvc")
+	  public ModelAndView showMVC() {
+		ModelAndView mv= new ModelAndView("view_mvc");
+		return mv;
+	}
+
+}
