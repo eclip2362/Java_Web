d3810a698ff332ecb0657d1e4fb390b9aa6ae919
Akali <Akali@My-PC>
2020-10-10 14:11:33 +0700

    Multiple View

---
+++ MultipleView/src/main/java/com/nguyenthanhhau/multipleview/controller/MVCController.java
@@ -0,0 +1,22 @@
+package com.nguyenthanhhau.multipleview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MVCController {
+  @RequestMapping("/mvc")
+  public ModelAndView showMVC() {
+		  ModelAndView mv=new ModelAndView("view_mvc");
+		  return mv;
+  }	  
+  @RequestMapping("/boot")
+   public ModelAndView showMVC1() {
+	  ModelAndView mv=new ModelAndView("view_boot");
+	  return mv;
+  }
+	  
+}
